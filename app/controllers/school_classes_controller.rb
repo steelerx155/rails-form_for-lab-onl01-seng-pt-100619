b5eec38dbb524d5d222d5a438dc8a7bb88fc482c
steelerx155 <steelerx155@gmail.com>
2020-03-15 16:36:49 -0400

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,26 @@
+class SchoolClassesController < ApplicationController
+  def new
+  end
+
+  def create
+    @school_class = SchoolClass.new(school_class_params)
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  
+  def update
+    @school_class = SchoolClass.new(school_class_params)
+    @school_class.save 
+    redirect_to school_class_path(@school_class)
+  end
+
+  def school_class_params
+    params.require(:school_class).permit!
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  resources :school_classes
+  resources :students 
+  # get 'school_classes/new,'
+
+  # get 'school_classes/create,'
+
+  # get 'school_classes/show,'
+
+  # get 'school_classes/edit,'
+
+  # get 'school_classes/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
