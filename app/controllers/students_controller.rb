efb46121f683a028b87a61753ec6a89e2ad63098
Randy Steele <steelerx155@gmail.com>
2020-03-16 17:59:23 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,7 +7,7 @@ class StudentsController < ApplicationController
     def create
       @student = Student.new(student_params)
       @student.save
-      redirect_to students_path(@student)
+      redirect_to student_path(@student)
     end
   
     def show
