d2c2a84e922210c362b66c51769cb51fa0c14002
Randy Steele <steelerx155@gmail.com>
2020-03-16 18:02:07 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,10 @@ class StudentsController < ApplicationController
       @student.save
       redirect_to student_path(@student)
     end
+    
+    def edit
+    @student = Student.find(params[:id])
+  end
   
     def show
         @student = Student.find(params[:id])
